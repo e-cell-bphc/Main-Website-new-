56eeeef9014a9b04908e1f73e06a59793995c3ea
Paritosh Kadam <priyatamkadam@gmail.com>
2022-02-25 11:57:28 +0530

    PP esummit

--- components/esummit/eventlist_esummit.js
+++ components/esummit/eventlist_esummit.js
@@ -1,45 +1,45 @@
 const events = [
   {
     id: 1,
-    heading: "Ground Reality",
-    link: "/groundreality",
+    heading: 'Ground Reality',
+    link: '/groundreality',
     description:
-      "One of our B-Plan competitions, Ground Reality, is organised to promote innovative start-ups and assist them. ",
+      'One of our B-Plan competitions, Ground Reality, is organised to promote innovative start-ups and assist them. '
   },
   {
     id: 2,
-    heading: "Beyond Profits",
-    link: "/beyondprofits",
+    heading: 'Beyond Profits',
+    link: '/beyondprofits',
     description:
-      "Our Social Business Plan competition, a one-of-its-kind contest. Aiming to foster innovation to solve social problems and encourage social entrepreneurship.",
+      'Our Social Business Plan competition, a one-of-its-kind contest. Aiming to foster innovation to solve social problems and encourage social entrepreneurship.'
   },
   {
     id: 3,
-    heading: "Pitchers Pilot",
-    link: "/pitchersPilot",
+    heading: 'Pitchers Pilot',
+    link: '/pitchersPilot',
     description:
-      "Start-up Expo is an initiative that provides young entrepreneurs with the spotlight they deserve by showcasing their enterprises to the attendees present at the summit",
+      "It provides a platform for seed-stage startups seeking funds and mentorship to pitch their ideas directly to our esteemed panel, who can choose to invest in the company based on their reasoning. "
   },
   {
     id: 4,
-    heading: "Internship Drive",
-    link: "/commingsoon",
+    heading: 'Internship Drive',
+    link: '/commingsoon',
     description:
-      "Internship Drive is an amazing opportunity to find an ideal internship, based on their skills or interests, from 30+ startups providing stipends from 3k to 30k INR.",
+      'Internship Drive is an amazing opportunity to find an ideal internship, based on their skills or interests, from 30+ startups providing stipends from 3k to 30k INR.'
   },
   {
     id: 5,
-    heading: "Competitions",
-    link: "/competitions",
+    heading: 'Competitions',
+    link: '/competitions',
     description:
-      "We are responsible for conducting dozens of competitions around the year based entirely to test an entrepreneurial mind and provide everyone with a chance to participate in cross-campus events.",
+      'We are responsible for conducting dozens of competitions around the year based entirely to test an entrepreneurial mind and provide everyone with a chance to participate in cross-campus events.'
   },
   {
     id: 6,
-    heading: "Workshop",
-    link: "/commingsoon",
+    heading: 'Workshop',
+    link: '/commingsoon',
     description:
-      "E-cell hosts several workshops which provide everyone with a chance to experience the entrepreneurial essence and culture of our college.",
-  },
-];
+      'E-cell hosts several workshops which provide everyone with a chance to experience the entrepreneurial essence and culture of our college.'
+  }
+]
 export default events;
