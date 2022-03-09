82826791a2ecc1229608577f2ee6c6703c635b31
Sanchit Kalra <sanchit.kalra1@gmail.com>
2022-03-09 12:38:27 +0530

    Fix companies not being rendered

--- components/id/companyCards.js
+++ components/id/companyCards.js
@@ -7,39 +7,47 @@ import axios from 'axios'
 
 function CompanyCards() {
   const { data: session, status } = useSession()
-  const [datas, setData] = useState([
+  const [datas, setData] = useState([])
+  useEffect(() => {
+    async function get() {
+      axios
+        .post('https://backend-api-2022.onrender.com/api/company/getCompany')
+        .then((res) => {
+          console.log(res.data)
+          setData(res.data)
+        })
+        .catch((err) => {
+          alert(err)
+        })
+    }
+    get()
+  }, [])
 
-  ])
-  useEffect(
-    async function datacall() {
-      axios.post('https://backend-api-2022.onrender.com/api/company/getCompany').then((res) => {
-        console.log(res.data)
-        setData(res.data)
-      }).catch((err) => {
-       alert(err)
-     })
-    }, [])
-
-    console.log(datas)
+  console.log(datas)
   function Auth({ prop }) {
-        if (status == 'authenticated') {
-          return (
-            <>
-              <div className={styles.button} 
-              >{prop.statuss}</div>
-            </>
-          )
-        } else return null
-      }
- 
+    if (status == 'authenticated') {
+      return (
+        <>
+          <div className={styles.button}>{prop.statuss}</div>
+        </>
+      )
+    } else return null
+  }
+
   return (
     <>
       {datas.map((data) => {
         return (
-          <>
+          <div key={data.name}>
             <div className={styles.cardContainer}>
               <div className={styles.image}>
-                {<Image src={launchpad} width={160} height={160} />}
+                {
+                  <Image
+                    src={`/assets/${data.logoURL}`}
+                    width={160}
+                    height={160}
+                  />
+                }
               </div>
               <div className={styles.props}>
                 <div className={styles.propitem}>
@@ -57,18 +65,14 @@ function CompanyCards() {
                   })}
                 </div>
                 <div className={styles.propitem}>
-                  <a
-                    className={styles.Eligibilty}
-                    href={data.companyDesc}
-                    download
-                  >
+                  <a className={styles.Eligibilty} href={data.companyDesc}>
                     Job Description
                   </a>
                 </div>
               </div>
               {/* <Auth prop={data} /> */}
             </div>
-          </>
+          </div>
         )
       })}
     </>
