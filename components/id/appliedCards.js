246fd567c0f1c48c6e57b7949ef229133ad6157d
Sanchit Kalra <sanchit.kalra1@gmail.com>
2022-03-16 18:43:23 +0530

    applied cards

---
+++ components/id/appliedCards.js
@@ -0,0 +1,98 @@
+import React, { useEffect, useState } from 'react'
+import styles from '../../styles/id/companyCards.module.css'
+import Image from 'next/image'
+import launchpad from '../../assets/Launchpad.png'
+import { useSession } from 'next-auth/react'
+import axios from 'axios'
+
+function AppliedCards() {
+  const { data: session, status } = useSession()
+  const [datas, setData] = useState([])
+  useEffect(() => {
+    console.log('use effect called')
+    function get() {
+      axios
+        .get(
+          'https://backend-api-2022.onrender.com/api/applications/getAppliedCompaniesUser',
+          {
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: 'Bearer ' + session.accessToken
+            }
+          }
+        )
+        .then((res) => {
+          console.log(res.data.data)
+          setData(res.data.data)
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    }
+    if (status == 'authenticated') {
+      get()
+    }
+  }, [status])
+
+  return (
+    <>
+      {datas.map((data, index) => {
+        return (
+          <div key={index}>
+            <div className={styles.cardContainer}>
+              <div className={styles.image}>
+                {
+                  <Image
+                    src={`/assets/${data.logoURL}`}
+                    width={160}
+                    height={90}
+                  />
+                }
+              </div>
+              <div className={styles.props}>
+                <div className={styles.propitem}>
+                  <a className={styles.companyName}>{data.name}</a>
+                </div>
+                <div className={styles.propitem}>
+                  <a className={styles.companyStipend} href={data.websiteLink}>
+                    {data.websiteLink}
+                  </a>
+                </div>
+                <div className={styles.headprop}>Roles</div>
+                <div className={styles.propitem1}>
+                  {data.roles.map((data) => {
+                    return (
+                      <a className={styles.Eligibilty} key={data._id}>
+                        {data}
+                      </a>
+                    )
+                  })}
+                </div>
+                <div className={styles.propitem}>
+                  <a
+                    className={styles.Eligibilty}
+                    href={data.companyDesc}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Job Description
+                  </a>
+                  {/* <div >
+                    <button className={styles.button}
+                      onClick={(e) => e.preventDefault(applyNow(data._id))}
+                    >
+                      Apply Now
+                    </button>
+                  </div> */}
+                </div>
+              </div>
+              {/* <Auth prop={data} /> */}
+            </div>
+          </div>
+        )
+      })}
+    </>
+  )
+}
+
+export default AppliedCards
--- pages/id/viewCompany.js
+++ pages/id/viewCompany.js
@@ -1,105 +1,96 @@
-import React, { useState } from 'react';
-import CompanyCards from '../../components/id/companyCards';
-import styles from "../../styles/id/viewcompany.module.css"
-
+import React, { useState } from 'react'
+import CompanyCards from '../../components/id/companyCards'
+import AppliedCards from '../../components/id/appliedCards'
+import styles from '../../styles/id/viewcompany.module.css'
 
 function ViewCompany() {
+  const [toggle, setToggle] = useState(1)
 
-  const [toggle, setToggle]=useState(1);
-
-  const handleClass = (toggle)=> {
+  const handleClass = (toggle) => {
     if (toggle == 1) {
-      return styles.cards;
-    }
-    else if (toggle == 2) {
-      return styles.cardsShift;
-    }
-    else if (toggle == 3) {
-      return styles.cardsBack;
+      return styles.cards
+    } else if (toggle == 2) {
+      return styles.cardsShift
+    } else if (toggle == 3) {
+      return styles.cardsBack
     }
   }
-  const handleClass2 = (toggle)=> {
+  const handleClass2 = (toggle) => {
     if (toggle == 1) {
-      return styles.cards2;
-    }
-    else if (toggle == 2) {
-      return styles.cardsShift2;
-    }
-    else if (toggle == 3) {
-      return styles.cardsBack2;
+      return styles.cards2
+    } else if (toggle == 2) {
+      return styles.cardsShift2
+    } else if (toggle == 3) {
+      return styles.cardsBack2
     }
   }
 
   const handlebtnClass1 = (toggle) => {
     if (toggle == 1 || toggle == 3) {
       return styles.leftSlide
-    }
-    else {
+    } else {
       return styles.null
     }
   }
 
-    const handlebtnClass2 = (toggle) => {
-      if (toggle == 2) {
-        return styles.rightSlide;
-      } else {
-        return styles.null;
-      }
-    };
+  const handlebtnClass2 = (toggle) => {
+    if (toggle == 2) {
+      return styles.rightSlide
+    } else {
+      return styles.null
+    }
+  }
 
-  const handleHead = (toggle)=> {
-    if (toggle == 1 ) {
-        return styles.heading1def
-    }
-    else if (toggle == 3) {
-        return styles.heading1;
-    }
-    else {
+  const handleHead = (toggle) => {
+    if (toggle == 1) {
+      return styles.heading1def
+    } else if (toggle == 3) {
+      return styles.heading1
+    } else {
       return styles.heading1shift
     }
   }
-  
-    const handleHead2 = (toggle) => {
-      if (toggle == 2) {
-        return styles.heading2;
-      } else {
-        return styles.heading2shift;
-      }
-    };
 
+  const handleHead2 = (toggle) => {
+    if (toggle == 2) {
+      return styles.heading2
+    } else {
+      return styles.heading2shift
+    }
+  }
 
-    return (
-      <div className={styles.container}>
-        <div className={styles.headings}>
-          <div className={handleHead(toggle)}>View Companies</div>
-          <div className={handleHead2(toggle)}>Applied Companies</div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.headings}>
+        <div className={handleHead(toggle)}>View Companies</div>
+        <div className={handleHead2(toggle)}>Applied Companies</div>
+      </div>
+      <div className={styles.AllCards}>
+        <div className={handleClass(toggle)}>
+          <CompanyCards />
         </div>
-        <div className={styles.AllCards}>
-          <div className={handleClass(toggle)}>
-            <CompanyCards />
-          </div>
-          <div className={handleClass2(toggle)}>
-            {/* <CompanyCards /> */}
-          </div>
-        </div>
-        <div
-          onClick={() => {
-            setToggle(2);
-          }}
-          className={handlebtnClass1(toggle)}
-        >
-          Applied Companies
-        </div>
-        <div
-          onClick={() => {
-            setToggle(3);
-          }}
-          className={handlebtnClass2(toggle)}
-        >
-          View Companies
+        <div className={handleClass2(toggle)}>
+          <AppliedCards />
         </div>
       </div>
-    );
+      <div
+        onClick={() => {
+          setToggle(2)
+        }}
+        className={handlebtnClass1(toggle)}
+      >
+        Applied Companies
+      </div>
+      <div
+        onClick={() => {
+          setToggle(3)
+        }}
+        className={handlebtnClass2(toggle)}
+      >
+        View Companies
+      </div>
+    </div>
+  )
 }
 
-export default ViewCompany;
+export default ViewCompany
