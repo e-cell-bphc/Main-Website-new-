628c0e157c15b1696091cf1a99d72d80204848c8
Ayush Bhauwala <ayushbhauwala07@gmail.com>
2023-01-21 10:33:07 +0530

    GR and BP Forms API changed
    Updated the spreadsheet API for GR and BP forms and opened registrations

--- components/forms/FormGR.js
+++ components/forms/FormGR.js
@@ -23,20 +23,20 @@ function FormGR() {
   }
   const handleSubmit = async (e) => {
     e.preventDefault()
-    // await fetch("https://api.apispreadsheets.com/data/vAoSb7hvrZ1N2iKP/",
-    // {
-    // 	method: 'POST',
-    // 	body: JSON.stringify({"data": data}),
-    // })
-    // 	.then((res) => {
-    // 		alert("Form Data Submitted :)");
-    // 	}).then(() => {
-    // 		window.location = "https://www.ecellbphc.in/groundreality";
-    // 	})
-    // 	.catch((err) => {
-    // 		alert("Form Data Not Submitted :( . Please Try Again");
-    // 	});
-    alert('Registrations have closed, hope to see you next year!')
+    await fetch('https://api.apispreadsheets.com/data/ueZvTHiIs7FYejyo/', {
+      method: 'POST',
+      body: JSON.stringify({ data: data })
+    })
+      .then((res) => {
+        alert('Form Data Submitted :)')
+      })
+      .then(() => {
+        window.location = 'https://www.ecellbphc.in/groundreality'
+      })
+      .catch((err) => {
+        alert('Form Data Not Submitted :( . Please Try Again')
+      })
+    // alert('Registrations have closed, hope to see you next year!')
     setData({ ...data, ...INITIAL_VALUES })
   }
 
