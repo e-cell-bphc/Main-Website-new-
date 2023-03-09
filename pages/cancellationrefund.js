6d1ee9df182451002d756e45e83901f1e4bcea30
Ayush Bhauwala <ayushbhauwala07@gmail.com>
2023-03-09 11:53:04 +0530

    Updated cancellation and shipping policies pages

--- pages/cancellationrefund.js
+++ pages/cancellationrefund.js
@@ -5,17 +5,101 @@ import Link from 'next/link'
 function cancellation() {
   return (
     <>
-      <Head>
-        <title>Cancellation and Refund Policy| E-Cell BPHC</title>
-      </Head>
       <div className={styles.outer}>
-        <h2 className={styles.heading}>Cancellation and Refund Policy</h2>
-        <div className={styles.para}>
-          <p>
-            All payments made on this website are final. In exceptional cases,
-            please contact us at ecell@hyderabad.bits-pilani.ac.in
-          </p>
-        </div>
+        <h1>Return and Refund Policy</h1>
+        <p>Last updated: March 09, 2023</p>
+        <p>Thank you for shopping at E-Cell BPHC.</p>
+        <p>
+          If, for any reason, You are not completely satisfied with a purchase
+          We invite You to review our policy on refunds and returns.
+        </p>
+        <p>
+          The following terms are applicable for any products that You purchased
+          with Us.
+        </p>
+        <h1>Interpretation and Definitions</h1>
+        <h2>Interpretation</h2>
+        <p>
+          The words of which the initial letter is capitalized have meanings
+          defined under the following conditions. The following definitions
+          shall have the same meaning regardless of whether they appear in
+          singular or in plural.
+        </p>
+        <h2>Definitions</h2>
+        <p>For the purposes of this Return and Refund Policy:</p>
+        <ul>
+          <li>
+            <p>
+              <strong>Company</strong> (referred to as either &quot;the
+              Company&quot;, &quot;We&quot;, &quot;Us&quot; or &quot;Our&quot;
+              in this Agreement) refers to E-Cell BPHC.
+            </p>
+          </li>
+          <li>
+            <p>
+              <strong>Goods</strong> refer to the items offered for sale on the
+              Service.
+            </p>
+          </li>
+          <li>
+            <p>
+              <strong>Orders</strong> mean a request by You to purchase Goods
+              from Us.
+            </p>
+          </li>
+          <li>
+            <p>
+              <strong>Service</strong> refers to the Website.
+            </p>
+          </li>
+          <li>
+            <p>
+              <strong>Website</strong> refers to E-Cell BPHC, accessible from{' '}
+              <a
+                href="http://www.ecellbphc.in"
+                rel="external nofollow noopener"
+                target="_blank"
+              >
+                http://www.ecellbphc.in
+              </a>
+            </p>
+          </li>
+          <li>
+            <p>
+              <strong>You</strong> means the individual accessing or using the
+              Service, or the company, or other legal entity on behalf of which
+              such individual is accessing or using the Service, as applicable.
+            </p>
+          </li>
+        </ul>
+        <h1>Your Order Cancellation Rights</h1>
+        <p>
+          The deadline for cancelling an Order is 7 days from the date on which
+          You received the Goods or on which a third party you have appointed,
+          who is not the carrier, takes possession of the product delivered.
+        </p>
+        <p>
+          In order to exercise Your right of cancellation, You must inform Us of
+          your decision by means of a clear statement. You can inform us of your
+          decision by:
+        </p>
+        <ul>
+          <li>By email: ecell@hyderabad.bits-pilani.ac.in</li>
+        </ul>
+        <p>
+          We will reimburse You no later than 14 days from the day on which We
+          receive the returned Goods. We will use the same means of payment as
+          You used for the Order, and You will not incur any fees for such
+          reimbursement.
+        </p>
+        <h2>Contact Us</h2>
+        <p>
+          If you have any questions about our Returns and Refunds Policy, please
+          contact us:
+        </p>
+        <ul>
+          <li>By email: ecell@hyderabad.bits-pilani.ac.in</li>
+        </ul>
       </div>
     </>
   )
--- pages/shippingdelivery.js
+++ pages/shippingdelivery.js
@@ -8,15 +8,52 @@ function shipping() {
       <Head>
         <title>Shipping and Deliveries| E-Cell BPHC</title>
       </Head>
-      <div className={styles.outer}>
-        <h2 className={styles.heading}>Shipping and Deliveries</h2>
-        <div className={styles.para}>
-          <p>
-            Payments made on this website will be proccessed within 48 hours. To
-            know more about your order email us at
-            ecell@hyderabad.bits-pilani.ac.in
-          </p>
-        </div>
+      <div>
+        <h1>Shipping Policy</h1>
+        E-Cell BPHC ("we" and "us") is the operator of
+        (https://www.ecellbphc.in) ("Website"). By placing an order through this
+        Website you will be agreeing to the terms below. These are provided to
+        ensure both parties are aware of and agree upon this arrangement to
+        mutually protect and set expectations on our service.
+        <h2>General</h2>
+        <p>
+          Subject to stock availability. We try to maintain accurate stock
+          counts on our website but from time-to-time there may be a stock
+          discrepancy and we will not be able to fulfill all your items at time
+          of purchase. In this instance, we will fulfill the available products
+          to you, and contact you about whether you would prefer to await
+          restocking of the backordered item or if you would prefer for us to
+          process a refund.
+        </p>
+        <h2>Shipping Costs</h2>
+        <p>
+          Shipping costs are calculated during checkout based on weight,
+          dimensions and destination of the items in the order. Payment for
+          shipping will be collected with the purchase. This price will be the
+          final price for shipping cost to the customer.
+        </p>
+        <h2>Returns</h2>
+        <p>
+          E-Cell BPHC will happily accept returns due to change of mind as long
+          as a request to return is received by us within 1 days of receipt of
+          item and are returned to us in original packaging, unused and in
+          resellable condition. Return shipping will be paid at the customers
+          expense and will be required to arrange their own shipping. Once
+          returns are received and accepted, refunds will be processed to store
+          credit for a future purchase. We will notify you once this has been
+          completed through email. (E-Cell BPHC) will refund the value of the
+          goods returned but will NOT refund the value of any shipping paid.
+        </p>
+        <h2>Items Out Of Stock</h2>
+        <p>
+          If an item is out of stock, we will cancel and refund the out-of-stock
+          items and dispatch the rest of the order.
+        </p>
+        <h2>Customer service</h2>
+        <p>
+          For all customer service enquiries, please email us at
+          ecell@hyderabad.bits-pilani.ac.in
+        </p>
       </div>
     </>
   )
